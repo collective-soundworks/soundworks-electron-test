5696bcec3f013944adec4cb0db1d2d70aedd63cd
b-ma <benjamin.matuszewski@gmail.com>
2022-08-09 14:36:37 +0200

    updated to match @soundworks/electron

--- .electron.js
+++ .electron.js
@@ -1,18 +1,22 @@
 const pkg = require('./package.json');
 
 const config = {
-  name: pkg.name,
-  productName: "Soundworks Test",
+  // do not touch these ones
   // keep versionning synchronized with the current repo
+  name: pkg.name,
   buildVersion: pkg.version,
+  // product infos
+  productName: "Soundworks Test",
+  author: "Ircam ISMM",
   appId: 'fr.ircam.ismm.electron-test',
-  // icon: './media/icon.png',
   // auto-update config
   publish: {
     provider: 'github',
     owner: 'collective-soundworks',
     repo: 'soundworks-electron-test',
   },
+  // uncomment to use app specific icon
+  // icon: './media/icon.png',
   // list of files or directories that we don't want to include in the binary
   // by default the whole application except the .git directory is copied
   exclude: [
